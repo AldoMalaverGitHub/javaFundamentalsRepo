423004e4dd6d5d37283eab4fe15496697bdb7e4c
DCA <DCA@DCA-304-SERV>
2018-02-03 19:12:21 -0500

    agregando nuevos archivos

---
+++ ManejoArchivos/src/manejoarchivos/LeerArchivo.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manejoarchivos;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ *
+ * @author DCA
+ */
+public class LeerArchivo {
+    
+    public void leerArchivo(){
+         String file = "E:\\manejoArchivos\\index.txt";
+         
+         try(InputStream fis = new FileInputStream(file)){
+             
+             int b = fis.read();             
+             System.out.println(b);
+             fis.close();
+             
+         }catch(FileNotFoundException ffe){
+           ffe.printStackTrace();
+         }catch(IOException ie){
+             ie.printStackTrace();
+         }
+    }
+    
+}
